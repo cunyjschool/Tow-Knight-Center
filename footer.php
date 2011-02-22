e8fd02caa41a9f685057a24b4879e0a4a7dafcf9
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-02-22 23:20:28 +0000

    More work on the basic design

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+
+	<div class="wrap">
+
+		<p class="copy">&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></p>
+	
+	</div><!-- END .wrap -->
+
+</div><!-- END .footer -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
