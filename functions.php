4279b01bcfc5559740d3a2192cc341206d56bb79
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-02-22 22:13:33 +0000

    First work on splash page theme

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+define( 'TOWKNIGHTCENTER_VERSION', '0.0' );
+
+if ( !class_exists( 'towknightcenter' ) ) {
+	
+class towknightcenter
+{
+
+	/**
+	 * __construct()
+	 */
+	function __construct() {
+		
+		add_action( 'init', array( &$this, 'enqueue_styles' ) );
+		
+	} // END __construct()
+	
+	/**
+	 * enqueue_styles()
+	 */
+	function enqueue_styles() {
+		
+		if ( !is_admin() ) { 
+			wp_enqueue_style( 'towknightcenter', get_bloginfo( 'template_directory' ) . '/style.css', false, TOWKNIGHTCENTER_VERSION );
+		}
+		
+	} // END enqueue_styles()
+	
+} // END class towknightcenter
+	
+} // END if ( !class_exists( 'towknightcenter' ) )
+
+$towknightcenter = new towknightcenter();
+
+
+
+
+
+
+?>
